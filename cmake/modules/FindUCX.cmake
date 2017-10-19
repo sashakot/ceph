28ca3fa210437dee6389afc61ee2c8182b7cc6ab
Vasily Philipov <vasilyf@mellanox.com>
2017-10-19 10:32:31 +0300

    msg/async/ucx: initial UCX lib configuration
    Signed-off-by: Vasily Philipov <vasilyf@mellanox.com>

--- cmake/modules/FindUCX.cmake
+++ cmake/modules/FindUCX.cmake
@@ -6,14 +6,22 @@
 # UCX_FOUND - True if ucx found.
 
 # sanity check
+
 message(STATUS "Looking for UCX in ${WITH_UCX}")
-find_path(UCX_INCLUDE_DIR NAMES ucp/api/ucp.h PATHS ${WITH_UCX}/include)
-find_library(UCP_LIB NAMES libucp.a PATHS ${WITH_UCX}/lib)
-message (STATUS "ucp lib ${UCP_LIB}")
-if (UCP_LIB)
-  set(UCX_LIBS "-L${WITH_UCX}/lib -lucp")
-endif (UCP_LIB)
+if (WITH_UCX AND EXISTS ${WITH_UCX})
+    find_path(UCX_INCLUDE_DIR NAMES ucp/api/ucp.h PATHS "${WITH_UCX}/include")
+    find_library(UCP_LIBRARY ucp HINTS "${WITH_UCX}/lib")
+    find_library(UCS_LIBRARY ucs HINTS "${WITH_UCX}/lib")
+endif ()
+
+message (STATUS "ucp lib ${UCP_LIBRARY}")
+message (STATUS "ucs lib ${UCS_LIBRARY}")
+
+if (UCP_LIBRARY)
+    set(UCX_LIBS
+        ${UCP_LIBRARY}
+        ${UCS_LIBRARY})
+endif (UCP_LIBRARY)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(UCX DEFAULT_MSG UCX_LIBS UCX_INCLUDE_DIR)
-
+find_package_handle_standard_args(ucx DEFAULT_MSG UCX_LIBS UCX_INCLUDE_DIR)
