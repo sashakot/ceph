ec66aceaf541651f8fe2080bbfaf36cf8a9b15e8
Vasily Philipov <vasilyf@mellanox.com>
2017-12-20 11:30:13 +0200

    msg/async/ucx: Use UCXDriver if UCX was requested
    Signed-off-by: Vasily Philipov <vasilyf@mellanox.com>

--- src/msg/async/ucx/UCXEvent.cc
+++ src/msg/async/ucx/UCXEvent.cc
@@ -3,29 +3,34 @@
 
 #include "common/errno.h"
 
+//#define dout_subsys ceph_subsys_ms
+
+//#undef dout_prefix
+//#define dout_prefix *_dout << "UCXDriver."
+
 int UCXDriver::init(EventCenter *c, int nevent)
 {
-	return 0;
+	return EpollDriver::init(c, nevent);
 }
 
 int UCXDriver::add_event(int fd, int cur_mask, int add_mask)
 {
-	return 0;
+	return EpollDriver::add_event(fd, cur_mask, add_mask);
 }
 
 int UCXDriver::del_event(int fd, int cur_mask, int delmask)
 {
-	return 0;
+	return EpollDriver::del_event(fd, cur_mask, delmask);
 }
 
 int UCXDriver::resize_events(int newsize)
 {
-	return 0;
+	return EpollDriver::resize_events(newsize);
 }
 
 int UCXDriver::event_wait(vector<FiredFileEvent> &fired_events, struct timeval *tvp)
 {
-    return 0;
+    return EpollDriver::event_wait(fired_events, tvp);
 }
 
 
--- src/msg/async/ucx/UCXEvent.h
+++ src/msg/async/ucx/UCXEvent.h
@@ -1,23 +1,22 @@
 #ifndef CEPH_UCXEVENT_H
 #define CEPH_UCXEVENT_H
 
-#include "msg/async/Event.h"
 #include "msg/async/Stack.h"
+#include "msg/async/EventEpoll.h"
 
-class UCXDriver : public EventDriver {
-  CephContext *cct;
+class UCXDriver : public EpollDriver {
+    private:
+        CephContext *cct;
 
-public:
+    public:
+        UCXDriver(CephContext *c): EpollDriver(c), cct(c) {}
+        virtual ~UCXDriver() {}
 
-  UCXDriver(CephContext *c): cct(c) {}
-  virtual ~UCXDriver() { }
-
-  int init(EventCenter *c, int nevent) override;
-  int add_event(int fd, int cur_mask, int add_mask) override;
-  int del_event(int fd, int cur_mask, int del_mask) override;
-  int resize_events(int newsize) override;
-  int event_wait(vector<FiredFileEvent> &fired_events, struct timeval *tp) override;
-  bool need_wakeup() { return false; }
+        int init(EventCenter *c, int nevent) override;
+        int add_event(int fd, int cur_mask, int add_mask) override;
+        int del_event(int fd, int cur_mask, int del_mask) override;
+        int resize_events(int newsize) override;
+        int event_wait(vector<FiredFileEvent> &fired_events, struct timeval *tp) override;
 };
 
 #endif //CEPH_UCXEVENT_H
