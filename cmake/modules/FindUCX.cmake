182863a106e8e19422adb78e98147edbf64305f9
Vasily Philipov <vasilyf@mellanox.com>
2018-04-12 12:51:42 +0300

    cmake: UCX cmake fixes
    Signed-off-by: Vasily Philipov <vasilyf@mellanox.com>

--- cmake/modules/FindUCX.cmake
+++ cmake/modules/FindUCX.cmake
@@ -9,15 +9,20 @@
 
 message(STATUS "Looking for UCX in ${WITH_UCX}")
 if (WITH_UCX AND EXISTS ${WITH_UCX})
-    find_path(UCX_INCLUDE_DIR NAMES ucp/api/ucp.h PATHS "${WITH_UCX}/include")
+    find_path(UCX_INCLUDE_DIR ucp/api/ucp.h HINTS "${WITH_UCX}/include")
     find_library(UCP_LIBRARY ucp HINTS "${WITH_UCX}/lib")
     find_library(UCS_LIBRARY ucs HINTS "${WITH_UCX}/lib")
 endif ()
+
+message (STATUS "ucx include ${UCX_INCLUDE_DIR}")
 
 message (STATUS "ucp lib ${UCP_LIBRARY}")
 message (STATUS "ucs lib ${UCS_LIBRARY}")
 
 if (UCP_LIBRARY)
+    if (WITH_UCX_DEBUG AND (${WITH_UCX_DEBUG} MATCHES "yes"))
+        set(UCX_DEBUG_FLAGS TRUE)
+    endif ()
     set(UCX_LIBS
         ${UCP_LIBRARY}
         ${UCS_LIBRARY})
@@ -25,3 +30,8 @@ endif (UCP_LIBRARY)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(ucx DEFAULT_MSG UCX_LIBS UCX_INCLUDE_DIR)
+
+mark_as_advanced(
+    UCX_LIBS
+    UCX_INCLUDE_DIR
+)
